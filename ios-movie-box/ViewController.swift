076ff0ffb83473b86fc1f2a26721e83d03237f6a
fatosdagdevir <fatma.dagdevir@gmail.com>
2025-03-20 14:41:16 +0100

    Initial Commit

---
+++ ios-movie-box/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-movie-box
+//
+//  Created by Fatma Dagdevir on 20.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-movie-boxUITests/ios_movie_boxUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ios_movie_boxUITestsLaunchTests.swift
+//  ios-movie-boxUITests
+//
+//  Created by Fatma Dagdevir on 20.03.25.
+//
+
+import XCTest
+
+final class ios_movie_boxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
