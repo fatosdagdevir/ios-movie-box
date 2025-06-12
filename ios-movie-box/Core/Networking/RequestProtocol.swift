15568657ddf84cfa34d2e9a53d9701344bebd217
fatosdagdevir <fatma.dagdevir@gmail.com>
2025-06-12 10:45:40 +0200

    bearer token removed

--- ios-movie-box/Core/Networking/RequestProtocol.swift
+++ ios-movie-box/Core/Networking/RequestProtocol.swift
@@ -16,7 +16,7 @@ public extension RequestProtocol {
     var headers: [String: String] {
         [
             HTTP.RequestHeaderKey.contentType.rawValue: "application/json",
-            HTTP.RequestHeaderKey.authorization.rawValue: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDVkZGU4YmYyMWFmYmJjNTY0Yjc0N2M5ZTIzNWQxYiIsIm5iZiI6MTc0MjY4MTg0MS4xODIwMDAyLCJzdWIiOiI2N2RmMzZmMWU0YTljODY5MDYwN2I4MDEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.GiiTThDOseMhANAIgHeE16mw8Yhs8KYF-l_vFPATpMg"
+            HTTP.RequestHeaderKey.authorization.rawValue: "Bearer XXX"
         ]
     }
 }
